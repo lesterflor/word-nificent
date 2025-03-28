94580c69602f75b832cf6c4908440b89436ba1d6
lesterflor <155178241+lesterflor@users.noreply.github.com>
2025-03-27 20:33:24 -0400

    add opengraph image

---
+++ app/opengraph-image.tsx
@@ -0,0 +1,52 @@
+import { ImageResponse } from 'next/og';
+import { readFile } from 'node:fs/promises';
+import { join } from 'node:path';
+
+// Image metadata
+export const alt = 'WordNificent';
+export const size = {
+	width: 1200,
+	height: 630
+};
+
+export const contentType = 'image/png';
+
+// Image generation
+export default async function Image() {
+	// Font loading, process.cwd() is Next.js project directory
+	const interSemiBold = await readFile(
+		join(process.cwd(), 'public/fonts/Inter_18pt-Regular.ttf')
+	);
+
+	return new ImageResponse(
+		(
+			// ImageResponse JSX element
+			<div
+				style={{
+					fontSize: 128,
+					background: 'blue',
+					width: '100%',
+					height: '100%',
+					display: 'flex',
+					alignItems: 'center',
+					justifyContent: 'center'
+				}}>
+				WordNificent
+			</div>
+		),
+		// ImageResponse options
+		{
+			// For convenience, we can re-use the exported opengraph-image
+			// size config to also set the ImageResponse's width and height.
+			...size,
+			fonts: [
+				{
+					name: 'Inter',
+					data: interSemiBold,
+					style: 'normal',
+					weight: 400
+				}
+			]
+		}
+	);
+}
