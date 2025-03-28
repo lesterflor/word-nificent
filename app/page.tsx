68e72916e4b5194524f910f77725a6e8738cfc8c
lesterflor <155178241+lesterflor@users.noreply.github.com>
2025-03-27 20:30:12 -0400

    beta test

--- app/page.tsx
+++ app/page.tsx
@@ -14,15 +14,23 @@ export default async function Home() {
 		redirect('/sign-in');
 	}
 
-	const words = await getRawWords(getRandomLetter());
+	const currentLetter = getRandomLetter();
+
+	const words = await getRawWords(currentLetter);
 
 	const shuffled = shuffle([...(words.data ?? [])]);
 
 	const wordList = (shuffled as GetRawWord[]) ?? [];
 
 	return (
-		<div className='flex flex-col gap-6'>
+		<div className='flex flex-col gap-4'>
 			<StatsHud />
+			<div className='text-center w-full flex flex-row items-center justify-center gap-1'>
+				<div>Words that start with</div>
+				<div className='flex flex-col items-center justify-center w-12 h-12 font-bold capitalize text-2xl p-2 rounded-full border-2'>
+					{currentLetter}
+				</div>
+			</div>
 
 			<div className='flex flex-col gap-4'>
 				<CarouselWords words={wordList} />
